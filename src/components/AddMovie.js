600bba748255969e384674dc54b2cf4a56e080c7
Emma <emma@Emmas-MacBook-Pro.local>
2021-02-03 15:37:10 -0700

    Implement movie add input and add button

---
+++ src/components/AddMovie.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// // class AddMovie extends React.Component {
+// //   constructor(props) {
+// //     super(props);
+
+// //     this.state = {
+
+// //     }
+// //   }
+
+
+// //   render () {
+// //     return (
+
+// //     );
+// //   }
+// // }
+
+
+// export default AddMovie;
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieList from '../../src/components/MovieList.js';
+import AddMovie from '../../src/components/AddMovie.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,36 +14,46 @@ class App extends React.Component {
         {title: 'Sunshine'},
         {title: 'Ex Machina'},
       ],
-      movieSearchText: ""
+      movieSearchText: "",
+      movieAddText: ""
     };
 
     this.goClick = this.goClick.bind(this);
+    this.addClick = this.addClick.bind(this);
     this.searchTextChange = this.searchTextChange.bind(this);
+    this.addTextChange = this.addTextChange.bind(this);
   }
 
   // on click of go
   goClick (e) {
     e.preventDefault();
-    console.log('has been clicked :)');
     // use movie search text to search through movies & provide whichever data we want
-    if (this.state.movies.filter((movie, i) => {
+    (this.state.movies.filter((movie, i) => {
       if (movie.title.toLowerCase().includes(this.state.movieSearchText.toLowerCase())) {
         return movie;
       }
-    }).length > 0) {
+    }).length > 0) ?
       this.setState({
         movies: this.state.movies.filter((movie, i) => {
           if (movie.title.toLowerCase().includes(this.state.movieSearchText.toLowerCase())) {
             return movie;
           }
         })
-      })
-
-    } else {
-      this.setState({
+      }) : this.setState({
         movies: [{title: "no movie by that name found"}]
-      })
-    }
+      });
+
+    document.getElementById('movie-search').value = '';
+  }
+
+
+  addClick (e) {
+    e.preventDefault();
+      this.setState({
+        movies: [...this.state.movies, {title: this.state.movieAddText}]
+      });
+
+    document.getElementById('movie-add').value = '';
   }
 
   // capture input from search bar when go is clicked & set state.movieSearchText equal to the value w/in search bar
@@ -52,13 +63,26 @@ class App extends React.Component {
     })
   }
 
+  addTextChange (e) {
+    this.setState({
+      movieAddText: e.target.value
+    })
+  }
+
 
   render(){
     return(
       <div className="app">
         <h2 className="movie-list-title">Movie List</h2>
-        <input type="text" placeholder="Search..." onChange={this.searchTextChange}/>
-        <button type="submit" onClick={this.goClick}>Go!</button>
+        <form>
+          <input type="text" placeholder="Add movie title here" id="movie-add" onChange={this.addTextChange}/>
+          <button type="submit" onClick={this.addClick}>Add!</button>
+        </form>
+
+        <form>
+          <input type="text" placeholder="Search..." id="movie-search" onChange={this.searchTextChange}/>
+          <button type="submit" onClick={this.goClick}>Go!</button>
+        </form>
         <MovieList movies={this.state.movies}/>
       </div>
   )}
