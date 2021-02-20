351b89bd4cda07c75a0fa81d18160fa9da6132f0
Emma <emma@Emmas-MacBook-Pro.local>
2021-02-20 15:01:54 -0700

    level 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {movies: []};
+    this.searchMovie = this.searchMovie.bind(this);
   }
 
   componentDidMount() {
@@ -19,10 +20,27 @@ class App extends React.Component {
     })
   }
 
+  getTitle(e) {
+    this.setState({[e.target.id]: e.target.value});
+  }
+
+  searchMovie(e) {
+    e.preventDefault();
+    //let searched = [];
+    let searched = this.state.movies.filter((movie) => {
+      return movie.title.toLowerCase().includes(this.state.searchBar.toLowerCase());
+    })
+    searched.length === 0 ? this.setState({movies: [{title: "movie not found :("}]}): this.setState({movies: searched});
+    document.getElementById("searchBar").value = ''
+  }
+
   render() {
     return (
       <div>
         <h2>Movie List</h2>
+        Search for a movie:
+        <input placeholder="Enter movie title" id="searchBar" onChange={this.getTitle.bind(this)}></input>
+        <button onClick={this.searchMovie}>Search</button>
         <MovieList movies={this.state.movies}/>
       </div>
     )
